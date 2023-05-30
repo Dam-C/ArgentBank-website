1b9656d9ecc980c49bcf829c83ee2cf86497c020
Dam-C <dams.c128@gmail.com>
2023-05-30 22:18:42 +0200

    components in progress

---
+++ ArgentBank/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Home from "./pages/home.jsx";
+import Footer from "./components/footer.jsx";
+import TopBanner from "./components/topBanner.jsx";
+
+function App() {
+  return (
+    <>
+      <TopBanner />
+      <Home />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ ArgentBank/src/components/topBanner.jsx
@@ -0,0 +1,24 @@
+import abLogo from "../assets/argentBankLogo.png";
+
+const TopBanner = () => {
+  return (
+    <nav className="main-nav">
+      <a className="main-nav-logo" href="./index.html">
+        <img
+          className="main-nav-logo-image"
+          src={abLogo}
+          alt="Argent Bank Logo"
+        />
+        <h1 className="sr-only">Argent Bank</h1>
+      </a>
+      <div>
+        <a className="main-nav-item" href="./sign-in.html">
+          <i className="fa fa-user-circle"></i>
+          Sign In
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default TopBanner;
